7f40dc14e614335899d5daac4f0ff611365a8151
ArinaKozh <arina.mayer66@mail.ru>
2022-12-07 00:24:10 +0300

    задача 29

---
+++ задача_29/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+*/
+
+
+int GetNumber(string message)
+{
+   int result = 0;
+   bool isCorrect = false;
+
+   while(!isCorrect)
+   {
+
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out result))
+    {
+        isCorrect = true;
+    } 
+    else
+    {
+        Console.WriteLine("Неверно ввели число");
+    }
+   } 
+   return result;
+}
+
+int[] ArrayInput()
+{
+    int []array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = GetNumber($"Введите {i+1}-ый элемент");
+    }
+    return array;
+}
+
+void ArrayOutput(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int []array = ArrayInput();
+ArrayOutput(array);
