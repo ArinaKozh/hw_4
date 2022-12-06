afc7f63ea19e4f50659ae667da59d11d03348fb9
ArinaKozh <arina.mayer66@mail.ru>
2022-12-06 23:52:40 +0300

    задача 27

---
+++ задача_27/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+*/
+
+string GetNumber(string message)
+{
+   int result = 0;
+   bool isCorrect = false;
+
+   while(!isCorrect)
+   {
+
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out result) && result>0)
+    {
+        isCorrect = true;
+    } 
+    else
+    {
+        Console.WriteLine("Неверно ввели число");
+    }
+   } 
+   return result.ToString();
+}
+
+int result = 0;
+string a = GetNumber("Введите число");
+foreach (char i in a)
+{
+    result += i - '0';
+}
+Console.WriteLine(result);
